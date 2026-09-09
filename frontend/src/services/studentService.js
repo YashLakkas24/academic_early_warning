4a9bca0937d5c6acdb71af8f400515203b5a1370
Yash Lakkas <yashnirjala24@gmail.com>
2026-09-09 20:52:34 +0530

    frontend url added

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -1,6 +1,6 @@
 import { getAuth } from "firebase/auth";
 import app from "../firebase";
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const auth = getAuth(app);
 async function getAuthHeaders(includeJson = false) {
   const currentUser = auth.currentUser;
